3fdc2d5aa1b1e87dca8b5e0c6f4c455da1e1a791
Irakli <i.dzigrashvili@gmail.com>
2017-11-21 23:24:20 +0400

    commit two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDesciptionComponent } from './product-desciption/product-desciption.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductDesciptionComponent
   ],
   imports: [
     BrowserModule,
